c4eaaabcd08491c53ab6dd4a484eb5750144aaa0
Zak Monnet <zakmonnet@gmail.com>
2020-09-01 15:04:50 -0500

    updated javascript. can select element, but classes are not currently updating

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,29 @@
 let today = moment().format("dddd[,] MMMM do");
+let hours = $("[data-hour]");
+let now = moment().hour();
+let each;
 
-console.log(today);
+ console.log($("[data-hour]")[0]);
 
 $("#currentDay").text(today);
+
+for (var i = 0; i < 24; i++) {
+    each = $("[data-hour]")[i];
+    if ($("[data-hour]")[i].getAttribute("data-hour") < now) {
+        each.addClass("past");
+    };
+    // } if ($([data-hour=i]) = hour) {
+    //     $([data-hour=i]).attr("class", present)
+    // } else {
+    //     $('[data-hour="i"]').attr("class", future)
+    // };
+};
+
+// $("textarea").each(function(index, value) {
+//     console.log(`textarea${index}: ${this.data}`);
+// })
+
+
+
+// Event Listener for save buttons
+//$(".saveBtn").click();
