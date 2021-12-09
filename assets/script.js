45464dc3be1f30464bd807d75eed18e7a5354d9f
Zak Monnet <zakmonnet@gmail.com>
2021-12-09 13:23:55 -0600

    fixed formating error so date displays correctly

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-let today = moment().format("dddd[,] MMMM do");
+let today = moment().format("dddd[,] MMMM Do");
 let hours = $("textarea");
 let now = moment().hour();
 let tasks = [];
@@ -14,57 +14,57 @@ $(".saveBtn").click(saveTask);
 $("#currentDay").text(today);
 
 $("textarea").each(function () {
-    tArea = this;
-    if (parseInt(tArea.dataset.hour) < now) {
-        $(tArea).addClass("past");
-    } if (parseInt(tArea.dataset.hour) === now) {
-        $(tArea).addClass("present");
-    } if (parseInt(tArea.dataset.hour) > now) {
-        $(tArea).addClass("future");
-    };
+  tArea = this;
+  if (parseInt(tArea.dataset.hour) < now) {
+    $(tArea).addClass("past");
+  }
+  if (parseInt(tArea.dataset.hour) === now) {
+    $(tArea).addClass("present");
+  }
+  if (parseInt(tArea.dataset.hour) > now) {
+    $(tArea).addClass("future");
+  }
 });
 
 //retrieves stored tasks from local storage
 getTask();
 function getTask() {
-    var storedTasks = JSON.parse(localStorage.getItem("tasks"));
+  var storedTasks = JSON.parse(localStorage.getItem("tasks"));
 
-    if (storedTasks !== null) {
-        tasks = storedTasks;
-    }
-};
+  if (storedTasks !== null) {
+    tasks = storedTasks;
+  }
+}
 
 //displays saved tasks
 displayTasks();
 function displayTasks() {
-    for (var i = 0; i < tasks.length; i++) {
-        let find = tasks[i].taskTime;
-        let content = tasks[i].taskContent
-        $("textarea").each(function() {
-            tArea = this;
-            console.log(this.id);
-            if (tArea.id === find) {
-                tArea.innerHTML = content;
-            };
-        });
-    };
-};
+  for (var i = 0; i < tasks.length; i++) {
+    let find = tasks[i].taskTime;
+    let content = tasks[i].taskContent;
+    $("textarea").each(function () {
+      tArea = this;
+      console.log(this.id);
+      if (tArea.id === find) {
+        tArea.innerHTML = content;
+      }
+    });
+  }
+}
 
 // will save task when save button is clicked
 function saveTask(event) {
-    event.preventDefault();
-    taskTime = event.target.dataset.hour;
-    console.log(taskTime);
-    taskContent = $(event.target.previousElementSibling).val();
-    console.log(taskContent);
+  event.preventDefault();
+  taskTime = event.target.dataset.hour;
+  console.log(taskTime);
+  taskContent = $(event.target.previousElementSibling).val();
+  console.log(taskContent);
 
-     addEntry(taskTime, taskContent);
-     localStorage.setItem("tasks", JSON.stringify(tasks));
-};
+  addEntry(taskTime, taskContent);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
 
 function addEntry(taskTime, taskContent) {
-    tasks.push({ taskTime, taskContent });
-    console.log(tasks)
-};
-
-
+  tasks.push({ taskTime, taskContent });
+  console.log(tasks);
+}
